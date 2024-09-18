f83a7345f426732ddd29bee99f6a71dc5aa22f16
jakubwisniewski <jakub.wisniewski@supermetrics.com>
2024-09-18 12:45:22 +0200

    feat: module 6; TODO-00 .. TODO-12 Aspect Oriented Programming

--- lab/22-aop/src/main/java/config/RewardsConfig.java
+++ lab/22-aop/src/main/java/config/RewardsConfig.java
@@ -1,11 +1,9 @@
 package config;
 
-import javax.sql.DataSource;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
+import org.springframework.context.annotation.Import;
 import rewards.RewardNetwork;
 import rewards.internal.RewardNetworkImpl;
 import rewards.internal.account.AccountRepository;
@@ -15,7 +13,10 @@ import rewards.internal.restaurant.RestaurantRepository;
 import rewards.internal.reward.JdbcRewardRepository;
 import rewards.internal.reward.RewardRepository;
 
+import javax.sql.DataSource;
+
 @Configuration
+@Import({AspectsConfig.class})
 public class RewardsConfig {
 
 	@Autowired
