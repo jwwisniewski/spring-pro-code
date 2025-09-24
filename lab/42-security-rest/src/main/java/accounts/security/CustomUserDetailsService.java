667f85b4ec81c7b8c8bed2703bb5cc63679b4a58
jakubwisniewski <jakub.wisniewski@supermetrics.com>
2025-09-24 11:22:09 +0200

    Spring Boot -A Closer Look Lab - module 7 - TODO 13-20

--- lab/42-security-rest/src/main/java/accounts/security/CustomUserDetailsService.java
+++ lab/42-security-rest/src/main/java/accounts/security/CustomUserDetailsService.java
@@ -23,6 +23,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 // - Comment the @Component annotation added in a previous task
 
 @Primary
+//@Component
 public class CustomUserDetailsService implements UserDetailsService {
 
 	private PasswordEncoder passwordEncoder;
@@ -34,18 +35,18 @@ public class CustomUserDetailsService implements UserDetailsService {
 	@Override
 	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
 		User.UserBuilder builder = User.builder();
-//     builder.username(username);
-//     builder.password(passwordEncoder.encode(username));
-//     switch (username) {
-//         case "mary":
-//             builder.roles("USER");
-//             break;
-//         case "joe":
-//             builder.roles("USER", "ADMIN");
-//             break;
-//         default:
-//             throw new UsernameNotFoundException("User not found.");
-//     }
+     builder.username(username);
+     builder.password(passwordEncoder.encode(username));
+     switch (username) {
+         case "mary":
+             builder.roles("USER");
+             break;
+         case "joe":
+             builder.roles("USER", "ADMIN");
+             break;
+         default:
+             throw new UsernameNotFoundException("User not found.");
+     }
 
 		return builder.build();
 	}
